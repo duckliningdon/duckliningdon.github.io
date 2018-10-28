3e0da865b81985d5431685d9df53df34e9d1d253
Duckliningdon <duckliningdon@gmail.com>
2018-10-27 23:39:50 -0500

    giant update

---
+++ disabled pages/home.h
@@ -0,0 +1,49 @@
+<!-- BOX 2 -->
+<div class="box2">
+<div class="containerforbox2">
+<div class="left2">
+<a href="cloud" class="image">
+<img src="https://i.imgur.com/VqVH36r.png" alt="">
+</a>
+
+<div class="profile">
+<p>Cloud</p>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="ubox2">
+<div class="line-separator2"></div>
+<div class="ucfb2">
+<p class="utext2">probably will change this in the future</p>
+</div>
+</div>
+
+
+<!-- BOX 3 -->
+<div class="box3">
+<div class="containerforbox3">
+<div class="left3">
+<a href="lab" class="image">
+<img src="https://i.imgur.com/cBJC2HP.png" alt="">
+</a>
+
+<div class="profile">
+<p>Extras</p>
+</div>
+</div>
+</a>
+</div>
+</div>
+<div class="ubox3">
+<div class="line-separator3"></div>
+<div class="ucfb3">
+<p class="utext3">oooh, my favorite part of the website</p>
+</div>
+</div>
+<div class="box4">
+<span class="banana">🍌</span>
+</div>
+</div>
+/*css*/
